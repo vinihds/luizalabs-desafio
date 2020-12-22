db46688b987f7afdc6c931a25d2c8970b9ee71c1
Vinicius Silveira <vinicius.silveira8@hotmail.com>
2020-12-21 23:32:48 -0300

    Initial implementation of Redis

--- src/main/java/br/com/luizalabs/desafio/cep/api/Application.java
+++ src/main/java/br/com/luizalabs/desafio/cep/api/Application.java
@@ -3,8 +3,10 @@ package br.com.luizalabs.desafio.cep.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableCaching
 @EntityScan(basePackages = {"br.com.luizalabs.desafio.cep.core.entity"})
 @EnableJpaRepositories(basePackages = {"br.com.luizalabs.desafio.cep.core.repository"})
 @SpringBootApplication(scanBasePackages = {"br.com.luizalabs.desafio.cep"})
--- src/main/java/br/com/luizalabs/desafio/cep/core/dto/CepDTO.java
+++ src/main/java/br/com/luizalabs/desafio/cep/core/dto/CepDTO.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class CepDTO {
+public class CepDTO implements Serializable {
 
     private String rua;
     private String bairro;
